bd18965cfa8c4fa7e7913ad49d3096927899a98f
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-21 17:32:29 +0700

    commit

---
+++ client/src/table.jsx
@@ -0,0 +1,22 @@
+const Table = ({ data }) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>email</th>
+        </tr>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
